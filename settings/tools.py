be699bec0ab0c5b81f1b70172ff152cb27aa81b3
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2022-12-27 14:05:59 +0330

    add comments to the tools.py

--- settings/tools.py
+++ settings/tools.py
@@ -1,18 +1,18 @@
 import json
 import os
 
-
+# read the template of the settings (all settings and their options)
 def read_template():
     with open("settings_option.json") as json_string:
         template_json = json.loads(json_string.read())
     return template_json
 
-
+# write the changed settings to the json file
 def write_settigs(settings):
     with open("settings.json", 'w') as json_file:
         json.dump(settings, json_file)
 
-
+# read the current settings and if doesn't exist initialize it
 def read_settings():
     if os.stat("settings.json").st_size == 0:
         settings = {"cursor": 0}
